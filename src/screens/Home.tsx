f54df149d638b6287fd5ef87fcff8026781ddded
ahmetyusufyabaneri <ahmetyusuf1yabaneri@gmail.com>
2025-06-07 18:57:28 +0300

    updated

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,41 @@
-import {FlatList, SafeAreaView} from 'react-native';
+import {Alert, FlatList, SafeAreaView} from 'react-native';
 import Header from '../components/Header';
 import Subheader from '../components/Subheader';
 import MovieSection from '../components/MovieSection';
 import {homeSections} from '../constants/homeSections';
+import messaging from '@react-native-firebase/messaging';
+import {useEffect} from 'react';
 
 const Home: React.FC = () => {
+  const requestUserPermission = async () => {
+    const authStatus = await messaging().requestPermission();
+
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+      await messaging().registerDeviceForRemoteMessages();
+      const token = await messaging().getToken();
+      console.log('Token:', token);
+    }
+  };
+
+  useEffect(() => {
+    const init = async () => {
+      await requestUserPermission();
+    };
+
+    init();
+
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new message here', JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <SafeAreaView className="container">
       <Header />
